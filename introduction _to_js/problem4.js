4e1c85a79f8f0cd732cbb16abb7fce3db9d7cf57
Deepak Kelodiya <anshukelodiya@gmail.com>
2023-08-29 20:07:47 +0530

    added first commit

---
+++ introduction _to_js/problem4.js
@@ -0,0 +1,10 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+let name = "Deepak Kelodiya";
+let school = "New Monalisa public School";
+let rollno = 12567555;
+let marks = 159;
+console.log(name,"\n",school,"\n",rollno,"\n",marks)
---
+++ introduction _to_js/two.js
@@ -0,0 +1,13 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Deepak";
+console.log(name);
+name = "Manoj Kumar";
+console.log(name);
+name = "Preeti Kelodiya";
+console.log(name);
